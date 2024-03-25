a8d78eaab093e5e7f2c22949610157ee0b3bfb12
jpsehyun <jpsehyun@gmail.com>
2024-03-26 02:23:54 +0800

    deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 
-const ExampleComponent = () => {
-  const [data, setData] = useState([]);
+// const ExampleComponent = () => {
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // Get the server URL from environment variables or use a default value
-    const apiUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
-    const fullUrl = `${apiUrl}/api/data`;
+//   useEffect(() => {
+//     // Get the server URL from environment variables or use a default value
+//     const apiUrl = process.env.REACT_APP_SERVER_URL || 'http://localhost:3001';
+//     const fullUrl = `${apiUrl}/api/data`;
     
 
-    axios.get(fullUrl)
-      .then(response => {
-        setData(response.data);
-        console.log('Data:', response.data); // Log the data to the console
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
+//     axios.get(fullUrl)
+//       .then(response => {
+//         setData(response.data);
+//         console.log('Data:', response.data); // Log the data to the console
+//       })
+//       .catch(error => {
+//         console.error('Error fetching data:', error);
+//       });
+//   }, []);
 
-  return (
-    <div>
-      <h2>Dummy Data from AWS db</h2>
-      <ul>
-        {data.map(item => (
-          <li key={item.id}>{item.city} {item.population}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h2>Dummy Data from AWS db</h2>
+//       <ul>
+//         {data.map(item => (
+//           <li key={item.id}>{item.city} {item.population}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
 
-export default ExampleComponent;
+// export default ExampleComponent;
